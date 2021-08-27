c59d2970bec978793202ac94e1e78b1e5360c857
Charles Roberts <cjroberts2013@gmail.com>
2021-08-27 15:15:16 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "chbcwharton",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "258223579",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
