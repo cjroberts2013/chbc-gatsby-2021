36b7fbd164ebeaa7cdc28756366b753f96b87827
Charles Roberts <cjroberts2013@gmail.com>
2021-08-28 16:50:38 -0400

    Fix icon

--- src/Components/Seo.js
+++ src/Components/Seo.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import {Helmet} from 'react-helmet';
 import {useStaticQuery, graphql} from 'gatsby';
+import Cross from './../images/cross.jpeg';
 
 export default function SEO({children, location, description, title, image}) {
     const { site } = useStaticQuery(graphql`
@@ -17,8 +18,8 @@ export default function SEO({children, location, description, title, image}) {
         <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
             <html lang="en" />
             <title>{title}</title>
-            <link rel="icon" type="image/jpeg" href="./../images/cross.jpeg" />
-            <link rel="alternate icon" href="./../images/cross.jpeg" />
+            <link rel="icon" type="image/jpeg" href={Cross} />
+            <link rel="alternate icon" href={Cross} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta charSet="utf-8"/>
             <meta name="description" content={site.siteMetadata.description} />
